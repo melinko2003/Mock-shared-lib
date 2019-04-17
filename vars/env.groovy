7f51769e00ecbe21cb3985e6c2c352221e525910
lcurran <lcurran@godaddy.com>
2019-04-17 17:00:51 -0500

    env v4

--- vars/env.groovy
+++ vars/env.groovy
@@ -2,13 +2,13 @@
 
 def call(options) {
 	
-	YUM_URL = 'https://yum.secureserver.net/api/v0.1/repos'
-        DEV_REPO = 'dev/centos/7/x86_64/pkims'
-        TEST_REPO = 'test/centos/7/x86_64/pkims'
-        SALT_MASTER = 'https://certlint-api.salt.int.godaddy.com:8000'
+	env.YUM_URL = 'https://yum.secureserver.net/api/v0.1/repos'
+        env.DEV_REPO = 'dev/centos/7/x86_64/pkims'
+        env.TEST_REPO = 'test/centos/7/x86_64/pkims'
+        env.SALT_MASTER = 'https://certlint-api.salt.int.godaddy.com:8000'
         #BUILD_DATE=new Date().format( 'yyyyMMddHHmm' )
-        RPM_PATH_BASE= './'
-        APP_NAME= "${options}"
+        env.RPM_PATH_BASE= './'
+        #env.APP_NAME= "${options}"
 	#RPM_NAME="${APP_NAME}-${BUILD_DATE}-1.git.${GIT_COMMIT}.x86_64.rpm"
 	
 }
