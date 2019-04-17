61b8e3030d4b9bf9ede8fb01f1194b022743f324
lcurran <lcurran@godaddy.com>
2019-04-17 17:29:37 -0500

    appEnv.groovy v2

--- vars/appEnv.groovy
+++ vars/appEnv.groovy
@@ -6,7 +6,8 @@ def call(options) {
         env.DEV_REPO = 'dev/centos/7/x86_64/pkims'
         env.TEST_REPO = 'test/centos/7/x86_64/pkims'
         env.SALT_MASTER = 'https://certlint-api.salt.int.godaddy.com:8000'
-        env.RPM_PATH_BASE= './'
-	
+        env.RPM_PATH_BASE = './'
+	env.BUILD_DATE = new Date().format( 'yyyyMMddHHmm' )	
+
 }
 
